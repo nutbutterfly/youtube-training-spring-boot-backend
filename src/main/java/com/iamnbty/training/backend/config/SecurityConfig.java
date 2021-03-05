1fd77581746b0b1a81a9cf88518235b897ce6185
Natthapon Pinyo <natthapon.p@aware.co.th>
2021-03-05 22:25:34 +0700

    update CORS

--- src/main/java/com/iamnbty/training/backend/config/SecurityConfig.java
+++ src/main/java/com/iamnbty/training/backend/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.iamnbty.training.backend.config;
 import com.iamnbty.training.backend.config.token.TokenFilterConfiguerer;
 import com.iamnbty.training.backend.service.TokenService;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,9 +11,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
-import java.util.Arrays;
+import java.util.Collections;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -25,8 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/actuator/**",
             "/user/register",
             "/user/login",
-            "/socket/**",
-            "/chat/**" // TODO: remove from config (Security Reason)
+            "/socket/**"
     };
 
     public SecurityConfig(TokenService tokenService) {
@@ -39,35 +36,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
-        // TODO:
-    }
-
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().disable().csrf().disable()
+        http.cors(config -> {
+            CorsConfiguration cors = new CorsConfiguration();
+            cors.setAllowCredentials(true);
+            cors.setAllowedOriginPatterns(Collections.singletonList("http://*"));
+            cors.addAllowedHeader("*");
+            cors.addAllowedMethod("GET");
+            cors.addAllowedMethod("POST");
+            cors.addAllowedMethod("PUT");
+            cors.addAllowedMethod("DELETE");
+            cors.addAllowedMethod("OPTIONS");
+
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", cors);
+
+            config.configurationSource(source);
+        }).csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().authorizeRequests().antMatchers(PUBLIC).anonymous()
                 .anyRequest().authenticated()
                 .and().apply(new TokenFilterConfiguerer(tokenService));
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-//        config.addAllowedOrigin("http://localhost:4200");
-        config.setAllowedOriginPatterns(Arrays.asList("http://localhost*"));
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("DELETE");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
     }
 
 }
--- src/main/java/com/iamnbty/training/backend/config/WebsocketConfig.java
+++ src/main/java/com/iamnbty/training/backend/config/WebsocketConfig.java
@@ -19,7 +19,7 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/socket")
-                .setAllowedOriginPatterns("http://localhost*")
+                .setAllowedOriginPatterns("http://*")
                 .withSockJS();
     }
 
